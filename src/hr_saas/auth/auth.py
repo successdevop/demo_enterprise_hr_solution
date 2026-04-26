1ccde503883a77633e49e575a7834ee64f797f8e
successdevop <successraphael28@gmail.com>
2026-04-26 18:13:12 +0100

    implemented the auth class where users/employees can register, login, logout, reset password

--- src/hr_saas/auth/auth.py
+++ src/hr_saas/auth/auth.py
@@ -1,22 +1,22 @@
 from typing import Optional
 from src.hr_saas.repository.employee_repo import EmployeeRepo
 from src.hr_saas.model.employee import Employee
-from src.hr_saas.error_handling.exceptions import UserAlreadyExistError
+from src.hr_saas.error_handling.exceptions import UserAlreadyExistError, ValidationError
 from src.hr_saas.utils.utils import Utils
 from src.hr_saas.file_IO.logging import Logger
-from src.hr_saas.file_IO.config_file import SUCCESS_LOG_FILE
+from src.hr_saas.file_IO.config_file import SUCCESS_LOG_FILE, ERROR_LOG_FILE, INFO_LOG_FILE
 
 
-class AuthN:
+class Auth:
     def __init__(self, emp_repo: EmployeeRepo):
-        self.emp_repo = emp_repo
-        self.current_user: Optional[Employee] = None
+        self._emp_repo = emp_repo
+        self._current_user: Optional[Employee] = None
 
     def register_user(self, name, email, age, origin, role, salary, password):
         email = email.strip().lower()
         Utils.validate_email(email)
 
-        if self.emp_repo.get_employee_by_email(email):
+        if self._emp_repo.get_employee_by_email(email):
             raise UserAlreadyExistError(f"{email} already exists")
 
         name = name.strip().title()
@@ -38,8 +38,55 @@ class AuthN:
 
         employee.set_password(password)
 
-        self.emp_repo.save_employee(employee)
+        self._emp_repo.save_employee(employee)
         Logger.success(f"Congratulations {name}, Registration Successful!!!", SUCCESS_LOG_FILE)
+        print("Registration successful")
 
         return employee
+
+    def login(self, email, password):
+        email = email.strip().lower()
+        Utils.validate_email(email)
+        user = self._emp_repo.get_employee_by_email(email)
+
+        if not user:
+            Logger.error(f"user not found | {email}", ERROR_LOG_FILE)
+            raise ValidationError("user not found")
+
+        if not user.check_password(password):
+            Logger.error(f"Invalid password for email | {email}", ERROR_LOG_FILE)
+            raise ValidationError("Invalid email or password")
+
+        if not user.isActive:
+            Logger.error(f"user account deactivated: {email}", ERROR_LOG_FILE)
+            raise ValidationError("Account deactivate, please contact support center/agents")
+
+        self._current_user = user
+        Logger.success(f"Login successful | {email}", SUCCESS_LOG_FILE)
+        print(f"Login successful | {email}")
+        return user
+
+    def logout(self):
+        if self._current_user:
+            Logger.info(f"{self._current_user.email} logged out", INFO_LOG_FILE)
+            print(f"{self._current_user.email} logged out")
+            self._current_user = None
+
+    def forgot_password(self, email):
+        email = email.strip().lower()
+
+        user = self._emp_repo.get_employee_by_email(email)
+        if not user:
+            Logger.error(f"Unknown/unregistered user trying to change password | {email}", ERROR_LOG_FILE)
+            print(f"{email} not found")
+
+        password = str(input("enter new password: "))
+        verify_password = str(input("verify new password: "))
+        if password == verify_password:
+            user.set_password(password)
+
+        self._emp_repo.save_employee(self._current_user)
+        Logger.info("Password changed successfully", INFO_LOG_FILE)
+        print("Password changed successfully")
+
 
