cfb87010607417d46a75fee6f014151cdfa7c881
successdevop <successraphael28@gmail.com>
2026-04-28 11:11:48 +0100

    implemented the apply for leave method

--- src/hr_saas/repository/leave_repo.py
+++ src/hr_saas/repository/leave_repo.py
@@ -1,6 +1,7 @@
 import json
-from typing import Dict, List
+from typing import Dict, List, Optional
 from src.hr_saas.model.leave_request import LeaveRequest
+from src.hr_saas.model.employee import Employee
 from src.hr_saas.file_IO.logging import Logger
 from src.hr_saas.file_IO.config_file import ERROR_LOG_FILE
 
@@ -11,13 +12,38 @@ class LeaveRepository:
         self._leave_database: Dict[str, List[LeaveRequest]] = {}
         self._load_leave_database()
 
+    def get_all_leave_request(self):
+        return self._leave_database
+
+    def get_employee_requests(self, email: str) -> List[LeaveRequest]:
+        return self._leave_database.get(email, [])
+
+    def save_leave_request(self, leave_request: LeaveRequest):
+        email = leave_request.employee.email
+
+        if email not in self._leave_database:
+            self._leave_database[email] = []
+
+        self._leave_database[email].append(leave_request)
+
+        # Serialization process
+        return self._persist_to_disk()
+
     def _load_leave_database(self):
         self._leave_database.clear()
+
+        if not self._leave_storage_file:
+            return
 
         try:
             with open(self._leave_storage_file, mode="r", encoding="utf-8") as file_reader:
                 data = json.load(file_reader)
                 print(data)
+
+                for email, requests_data in data.items():
+                    self._leave_database[email] = [
+                        LeaveRequest.from_to_dict(request) for request in requests_data
+                    ]
         except FileNotFoundError as e:
             # First run - file does not exist yet
             Logger.error(f"Database file not found, starting fresh | {e}", ERROR_LOG_FILE)
@@ -29,3 +55,17 @@ class LeaveRepository:
         except Exception as e:
             Logger.error(f"Error loading Leave_Request database | {e}", ERROR_LOG_FILE)
             self._leave_database = {}
+
+    def _persist_to_disk(self):
+        savable_data = {
+            email: [req.to_dict() for req in requests]
+            for email, requests in self._leave_database.items()
+        }
+
+        try:
+            with open(self._leave_storage_file, mode="w", encoding="utf-8") as file_writer:
+                json.dump(savable_data, file_writer, indent=4)
+            return True
+        except Exception as e:
+            Logger.error(f"Error saving leave_request | {e}", ERROR_LOG_FILE)
+            return False
