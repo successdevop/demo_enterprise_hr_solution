3f4e23e02bf668bb161f9d73615da0c4148cae5b
successdevop <successraphael28@gmail.com>
2026-04-29 12:11:33 +0100

    remodified the data structure for our leave database to improve efficiency and also added some methods to the repository

--- src/hr_saas/repository/leave_repo.py
+++ src/hr_saas/repository/leave_repo.py
@@ -9,24 +9,45 @@ from src.hr_saas.file_IO.config_file import ERROR_LOG_FILE
 class LeaveRepository:
     def __init__(self, storage_file):
         self._leave_storage_file = storage_file
-        self._leave_database: Dict[str, List[LeaveRequest]] = {}
+        self._leave_database: Dict[str, Dict[str, LeaveRequest]] = {}
         self._load_leave_database()
 
     def get_all_leave_request(self):
         return self._leave_database
 
-    def get_employee_requests_by_email(self, email: str) -> List[LeaveRequest]:
-        return self._leave_database.get(email, [])
+    def get_employee_requests_by_email(self, email: str) -> Dict[str, LeaveRequest]:
+        return self._leave_database.get(email, {})
+
+    def get_leave_balance(self, email: str) -> Optional[int]:
+        if email not in self._leave_database:
+            print("Employee has no previous leave_request")
+            return None
+
+        if not self._leave_database.get(email):
+            print("Employee has no previous leave_request")
+            return None
+
+        total_leave_requested = 0
+        total_available_leave_days = 0
+        for requests in self._leave_database.get(email).values():
+            total_available_leave_days = requests.total_leave_for_the_year
+            if requests.leave_status.value == "Pending" or requests.leave_status.value == "Approved":
+                total_leave_requested += requests.days
+
+        balance = total_available_leave_days - total_leave_requested
+        if balance == 0:
+            raise ValueError("You have used up your leave for the year")
+
+        return balance
 
     def save_leave_request(self, leave_request: LeaveRequest):
         email = leave_request.employee.email
 
         if email not in self._leave_database:
-            self._leave_database[email] = []
+            self._leave_database[email] = {}
 
-        user = next((leave for leave in self._leave_database.get(email) if leave.leave_id))
-        if not user:
-            self._leave_database[email].append(leave_request)
+        # if leave_request.leave_id not in self._leave_database.get(email):
+        self._leave_database[email][leave_request.leave_id] = leave_request
 
         # Serialization process
         return self._persist_to_disk()
@@ -42,9 +63,10 @@ class LeaveRepository:
                 data = json.load(file_reader)
 
                 for email, requests_data in data.items():
-                    self._leave_database[email] = [
-                        LeaveRequest.from_to_dict(request) for request in requests_data
-                    ]
+                    self._leave_database[email] = {}
+                    for leave_id, leave_obj in requests_data.items():
+                        self._leave_database[email][leave_id] = LeaveRequest.from_to_dict(leave_obj)
+
         except FileNotFoundError as e:
             # First run - file does not exist yet
             Logger.error(f"Database file not found, starting fresh | {e}", ERROR_LOG_FILE)
@@ -59,7 +81,11 @@ class LeaveRepository:
 
     def _persist_to_disk(self):
         savable_data = {
-            email: [req.to_dict() for req in requests]
+            email: {
+                leave_id: req.to_dict()
+                for leave_id, req in requests.items()
+            }
+
             for email, requests in self._leave_database.items()
         }
 
