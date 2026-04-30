6e5db50b4e1d69c63dfb7e5db51b4496ad77468c
successdevop <successraphael28@gmail.com>
2026-04-30 09:41:59 +0100

    implemented the monthly enum class

---
+++ src/hr_saas/enums/month.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Month(Enum):
+    JANUARY = "January"
+    FEBRUARY = "February"
+    MARCH = "March"
+    APRIL = "April"
+    MAY = "May"
+    JUNE = "June"
+    JULY = "July"
+    AUGUST = "August"
+    SEPTEMBER = "September"
+    OCTOBER = "October"
+    NOVEMBER = "November"
+    DECEMBER = "December"
