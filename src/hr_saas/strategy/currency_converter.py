b3daba9841f86f80133d683c53eeca2fc4799049
successdevop <successraphael28@gmail.com>
2026-04-30 16:23:44 +0100

    implemeted the currency converter strategy

---
+++ src/hr_saas/strategy/currency_converter.py
@@ -0,0 +1,18 @@
+class CurrencyStrategy:
+    def __repr__(self):
+        self.rates = {
+            "NGN": 1,
+            "USD": 0.0007,
+            "EUR": 0.0006,
+            "GBP": 0.0005
+        }
+
+    def convert(self, amount, from_currency, to_currency):
+        if from_currency not in self.rates or to_currency not in self.rates:
+            print("Currency not supported")
+            return
+
+        base = amount / self.rates[from_currency]
+        return base * self.rates[to_currency]
+
+
