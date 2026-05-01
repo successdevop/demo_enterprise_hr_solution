211000e9de7a81a692ad0de4bdb51306cd43be0b
successdevop <successraphael28@gmail.com>
2026-05-01 13:41:58 +0100

    implemented the attendance repository with the save, load and get attendance method

---
+++ src/hr_saas/repository/attendance_repo.py
@@ -0,0 +1,69 @@
+import json
+from typing import Dict, List, Optional
+from datetime import datetime
+from src.hr_saas.model.attendance import Attendance
+from src.hr_saas.model.employee import Employee
+from src.hr_saas.file_IO.logging import Logger
+from src.hr_saas.file_IO.config_file import ERROR_LOG_FILE
+
+
+class AttendanceRepository:
+    def __init__(self, storage_file: str):
+        self._attendance_storage_file = storage_file
+        self._attendance_database: Dict[str, List[Attendance]] = {}
+        self._load_attendance_database()
+
+    def get_employee_attendance(self, employee: Employee) -> Optional[Attendance]:
+        attendance_history = self._attendance_database.get(employee.email, [])
+        for attendance in attendance_history:
+            if attendance.date.day == datetime.today().day:
+                return attendance
+        return None
+
+    def save_attendance(self, attendance: Attendance):
+        email = attendance.employee.email
+        if email not in self._attendance_database:
+            self._attendance_database[attendance.employee.email] = []
+
+        self._attendance_database.get(email).append(attendance)
+
+        return self._save_to_disk()
+
+    def _save_to_disk(self):
+        savable_data = {
+            email: [attendance.to_dict() for attendance in attendance_list]
+            for email, attendance_list in self._attendance_database.items()
+        }
+
+        try:
+            with open(self._attendance_storage_file, mode="w", encoding="utf-8") as file_writer:
+                json.dump(savable_data, file_writer, indent=4)
+                return True
+        except Exception as e:
+            Logger.error(f"Error saving Attendance data | {e}", ERROR_LOG_FILE)
+            return False
+
+    def _load_attendance_database(self):
+        self._attendance_database.clear()
+
+        if not self._attendance_storage_file:
+            print("Attendance json file not found")
+            return
+
+        try:
+            with open(self._attendance_storage_file, mode="r", encoding="utf-8") as file_reader:
+                data = json.load(file_reader)
+
+                if isinstance(data, dict):
+                    for email, attendance_data in data.items():
+                        self._attendance_database[email] = [
+                            Attendance.from_to_dict(attendance) for attendance in attendance_data
+                        ]
+        except FileNotFoundError as e:
+            # First run - database file doesn't exist
+            Logger.error(f"Database file does not exist yet, starting fresh | {e}", ERROR_LOG_FILE)
+        except json.JSONDecodeError as e:
+            # Database file exists but either empty or corrupted
+            Logger.error(f"JSON Decode Error | {e}", ERROR_LOG_FILE)
+        except Exception as e:
+            Logger.error(f"Error loading attendance_database | {e}", ERROR_LOG_FILE)
